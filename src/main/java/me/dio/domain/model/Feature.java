3885773938e8d1ce138423340de6a131cf8b0c61
William Silva <william.silva@viannasempre.com.br>
2024-07-29 18:21:33 -0300

    Implementação da camada de domínio

---
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,13 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+/**
+ * @author William
+ * @date 29/07/2024
+ */
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/domain/model/News.java
@@ -0,0 +1,13 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+/**
+ * @author William
+ * @date 29/07/2024
+ */
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
